7563c31a42e864ebef86e2f6d03d3e802211760f
bondhan <bondhan@texo.id>
2019-02-19 11:27:15 +0700

    init

---
+++ matrix/matrix.go
@@ -0,0 +1,15 @@
+package matrix
+
+import "strings"
+
+func New(inputNumbers string) {
+	rows := strings.Split(inputNumbers, "\n")
+
+	rowInt := 0
+	colInt := 0
+	for index, row := range rows {
+		rowValue := strings.Split(row, " ")
+
+	}
+
+}
