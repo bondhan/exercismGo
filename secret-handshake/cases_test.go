332190ecc87dd8a87241b9de87d45d0adf34ab4e
bondhan <bondhan@texo.id>
2019-03-23 20:53:18 +0700

    finish secret handshake

--- secret-handshake/cases_test.go
+++ secret-handshake/cases_test.go
@@ -22,24 +22,3 @@ var tests = []secretHandshakeTest{
 	{31, []string{"jump", "close your eyes", "double blink", "wink"}},
 	{0, []string{}},
 }
-
-var consts = []string{"", "wink", "double blink", "close your eyes", "jump"}
-
-func Handshake(code uint) []string {
-	var compile []string
-
-	var index uint
-	for index = 1; index <= 16; index++ {
-		if (code & index) == uint(1) {
-			compile = append(compile, consts[1])
-		} else if (code & index) == uint(2) {
-			compile = append(compile, consts[2])
-		} else if (code & index) == uint(4) {
-			compile = append(compile, consts[3])
-		} else if (code & index) == uint(8) {
-			compile = append(compile, consts[3])
-		}
-	}
-
-	return compile
-}
---
+++ secret-handshake/secret_handshake.go
@@ -0,0 +1,33 @@
+package secret
+
+import "math"
+
+var consts = []string{"", "wink", "double blink", "close your eyes", "jump"}
+
+func reverseSlice(input []string) []string {
+	if len(input) == 0 {
+		return input
+	}
+	return append(reverseSlice(input[1:]), input[0])
+}
+
+func Handshake(code uint) []string {
+	var compile []string
+
+	for index := 0; index <= 4; index++ {
+		switch value := code & uint(math.Pow(2, float64(index))); value {
+		case uint(1):
+			compile = append(compile, consts[1])
+		case uint(2):
+			compile = append(compile, consts[2])
+		case uint(4):
+			compile = append(compile, consts[3])
+		case uint(8):
+			compile = append(compile, consts[4])
+		case uint(16):
+			compile = reverseSlice(compile)
+		}
+	}
+
+	return compile
+}
