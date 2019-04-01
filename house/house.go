71394f6f609e8c45b182ab9cc1eadfe28ea6d60a
bondhan <bondhan@texo.id>
2019-04-01 18:06:35 +0700

    update house

---
+++ house/house.go
@@ -0,0 +1,21 @@
+package house
+
+type text struct {
+	thing string
+}
+
+var texts = []text{
+	{"This is"},
+	{"the house that Jack built."},
+	{"the malt\nthat lay in"},
+	{"the rat\nthat ate "},
+	{"the cat\nthat killed "},
+	{"the dog\nthat worried "},
+	{"the cow with the crumpled horn\nthat tossed "},
+	{"the maiden all forlorn\nthat milked "},
+	{"the man all tattered and torn\nthat kissed "},
+	{"the priest all shaven and shorn\n that married "},
+	{"the rooster that crowed in the morn\nthat woke "},
+	{"the farmer sowing his corn\nthat kept "},
+	{"the horse and the hound and the horn\nthat belonged to "},
+}
