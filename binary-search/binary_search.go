8a49802f2d25ed64f7b4fe80e2d9571c2fbc58e0
bondhan@texo.id <bondhan.novandy@gmail.com>
2019-12-09 23:28:08 +0700

    update

--- binary-search/binary_search.go
+++ binary-search/binary_search.go
@@ -3,20 +3,28 @@ package binarysearch
 // SearchInts ...
 func SearchInts(slice []int, key int) int {
 
-	half := len(slice) / 2
+	if len(slice) < 1 {
+		return -1
+	}
 
-	isFound := false
-	for !isFound {
+	low := 0
+	high := len(slice) - 1
+	half := (low + high) / 2
 
+	for true {
+
+		//check if it is at the middle
 		if key == slice[half] {
 			return half
-		} else if key > slice[half] {
-			half = (half + len(slice)) / 2
-		} else if key < slice[half] {
-			half = half / 2
+		} else if key > slice[half] { // if key larger than the middle, make starting point from the half
+			low = half + 1
+		} else if key < slice[half] { // if key is smaller, make end point is the half
+			high = half - 1
 		}
 
-		if (half == 0 || half == len(slice)-1) && key != slice[half] {
+		half = (low + high) / 2 //compute the new half
+
+		if low > high {
 			break
 		}
 	}
