cbaf3525ba2f0ebd8bcabba4db0b72ba805ac6f0
Bondhan Novandy <bondhan.novandy@gmail.com>
2019-12-09 20:52:06 +0700

    binary search v1

--- binary-search/binary_search.go
+++ binary-search/binary_search.go
@@ -1,1 +1,23 @@
 package binarysearch
+
+// SearchInts ...
+func SearchInts(slice []int, key int) int {
+
+	isFound := false
+	for !isFound {
+		half := len(slice) / 2
+		if key == slice[half] {
+			return half
+		} else if key > slice[half] {
+			return half + SearchInts(slice[half:], key)
+		} else if key < slice[half] {
+			return SearchInts(slice[:half], key)
+		}
+
+		if half == 1 {
+			break
+		}
+	}
+
+	return 0
+}
